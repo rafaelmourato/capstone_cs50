fb642895da6ae990e3b0f265719f995fbd47b65f
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2026-02-19 22:53:51 -0300

    Starting the project

---
+++ ShoppingList/listsapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class List(models.Model):
+    pass
+
+class Supermarket(models.Model):
+    pass
+
+class User(models.Model):
+    pass
+
+class Products(models.Model):
+    pass
