03ec713608c714830c4d525b4eea07e17dca4b90
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2026-02-26 05:54:54 -0300

    created all the get views, only to be able to se the pages and test my models, now i'm going to start creating the logic to add, create, remove and others

--- ShoppingList/listsapp/urls.py
+++ ShoppingList/listsapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("user", views.userpage, name="userpage"),
+    path("list/<int:list_id>/", views.listpage, name="listpage"),
+    path("supermarket/<int:supermarket_id>/", views.supermarketpage, name="supermarketpage"),
+    path("product/<int:product_id>/", views.productpage, name="productpage"),
 ]
--- ShoppingList/listsapp/views.py
+++ ShoppingList/listsapp/views.py
@@ -1,23 +1,60 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.db import IntegrityError
-from .models import User
+from .models import User, Product, List, PriceMkt
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 def index(request):
-    return render(request, "listsapp/index.html") 
+    supermarkets = User.objects.all().filter(is_supermarket=True)
+    products = Product.objects.all().order_by("name")
+    listuser = None
+    if request.user.is_authenticated :
+        listuser = List.objects.filter(user=request.user)
+    return render(request, "listsapp/index.html",{
+        "supermarkets": supermarkets,
+        "list": listuser,
+        "products": products
+    }) 
 
-def listpage(request):
-    pass
+@login_required
+def listpage(request, list_id):
+    list_obj = get_object_or_404(List, id=list_id, user=request.user)
+    products = list_obj.products.all().order_by("name")
 
-def supermarketpage(request):
-    pass
+    return render(request, "listsapp/listpage.html", {
+        "list": list_obj,
+        "products": products
+    })
 
-def productpage(request):
-    pass
+@login_required
+def userpage(request):
+    lists = List.objects.filter(user=request.user)
+    return render(request, "listsapp/userpage.html", {
+        "lists": lists
+    })
+
+def supermarketpage(request, supermarket_id):
+    supermarket = get_object_or_404(
+        User,
+        id=supermarket_id,
+        is_supermarket=True
+    )
+    prices = PriceMkt.objects.filter(supermarket=supermarket).select_related("product")
+    return render(request, "listsapp/supermarketpage.html", {
+        "supermarket": supermarket,
+        "prices": prices
+    })
+
+def productpage(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, "listsapp/productpage.html", {
+        "product": product
+    })
 
 def login_view(request):
     if request.method == "POST":
